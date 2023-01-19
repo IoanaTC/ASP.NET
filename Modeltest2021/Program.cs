a6422dcf1aa2dc3af984407d81f341fcfd5dae4b
IoanaTC <ioanateodoraconstantin@yahoo.com>
2023-01-19 16:39:26 +0200

    Add project files.

---
+++ Modeltest2021/Program.cs
@@ -0,0 +1,33 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// conexiunea cu baza de date
+// extragem stringul de conexiune, stocat in appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
